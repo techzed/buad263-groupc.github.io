8127e9a7f7a02ebe7089d284452141d0f17b050d
techzed <79388325+techzed@users.noreply.github.com>
2021-02-20 23:30:50 -0800

    Update end.js

--- end.js
+++ end.js
@@ -8,7 +8,18 @@ const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 const MAX_HIGH_SCORES = 5
 
 finalScore.innerText = mostRecentScore
-
+meme_img.style.paddingBottom = '10px';
+if (mostRecentScore == 0){
+    meme_img.setAttribute('src', '../game/score-0.jpg')
+} else if (mostRecentScore >= 1 && mostRecentScore <= 800){
+    meme_img.setAttribute('src', '../game/poor.jpg')
+} else if (mostRecentScore >= 801 && mostRecentScore <= 1600){
+    meme_img.setAttribute('src', '../game/pass.jpg')
+} else if (mostRecentScore >= 1601 && mostRecentScore <= 2400){
+    meme_img.setAttribute('src', '../game/not-bad.jpg')
+} else if (mostRecentScore > 2400){
+    meme_img.setAttribute('src', '../game/awesome.jpg')
+}
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value
 })
